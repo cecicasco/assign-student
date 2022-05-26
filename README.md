5c8e3d7f9af2ef59c0941646c09d6cce5b671f31
Ceci Casco <cecilia.casco7@gmail.com>
2022-05-26 19:19:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# assign-Student
+Assign Students to educational establishment
+
+Asignación de Estudiantes a Establecimientos Educativos: Un Enfoque Multi-objetivo
+
+Cecilia Casco
+
+# Data Set
+To configure the data set used, perform the following tasks:
+  * Install PostgreSQL
+  * Create a user tfm with password Tfm123456 (Roles: Login and Create Database)
+  * Create a database tfmdb (owner tfm)
+  * Create a schame tfm (CREATE SCHEMA tfm AUTHORIZATION tfm;)
+  * Connect with the user created
+  * run the script.sql file
+
+# Run the algorithm
+py assignTeacher.py
---
+++ src/constraint.py
@@ -0,0 +1,23 @@
+import data
+
+#Compute all restrictions
+def validateConstraints(X):    
+    valid_c1=0
+    valid_c2 = 0
+    n_p=len(data.P)
+    for j in range(n_p):
+        c=1
+        for l in range(n_p):
+            if(j!=l and X[j]==X[l]):
+                c=c+1
+                class_index = X[j]
+                if(c>data.C[class_index][5]):#The number of assigments should be less o equal que capacity
+                    valid_c1=1
+                    break
+
+                if(data.P[l][3] != data.C[class_index][0]):  #The class should be the same grade as the student requered
+                    valid_c2 = 1
+                    break
+
+            
+    return [valid_c1, valid_c2]
