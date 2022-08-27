92428f16ba88a46ecfb5e64a1ff8dc2e43708a3a
cecicasco <cecilia.casco7@gmail.com>
2022-08-26 23:58:36 -0400

    Update constraint.py
    Add #The number of assigments should be less  40 and more than 10

--- src/constraint.py
+++ src/constraint.py
@@ -4,6 +4,7 @@ import datadb as data
 def validateConstraints(X):    
     valid_c1=0
     valid_c2 = 0
+    valid_c3 = 0
     n_p=len(data.P)
     for j in range(n_p):
         c=1
@@ -11,14 +12,14 @@ def validateConstraints(X):
         if (data.P[j][3] != data.C[class_index][0]):  # The class should be the same grade as the student requered
             valid_c1 = 1
             break
-        if  not X[j] : #All the students should be assign
+        if not X[j] : #All the students should be assign
             valid_c2 = 1
-  ##      for l in range(n_p):
-  #          if(j!=l and X[j]==X[l]):
-  #              c=c+1
-  #              if(c>data.C[class_index][5]): #The number of assigments should be less o equal que capacity
-  #                  valid_c2=1
-  #                  break
+        for l in range(n_p):
+          if(j!=l and X[j]==X[l]):
+              c=c+1
+              if(c>=10 and c<=40): #The number of assigments should be less  40 and more than 10
+                  valid_c3=1
+                  break
 
 
-    return [valid_c1, valid_c2]
+    return [valid_c1, valid_c2, valid_c3]
